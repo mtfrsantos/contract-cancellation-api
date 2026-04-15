3ff1fc2420539403aaf086588cfdc03f6100a738
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-04-15 16:10:14 -0300

    feat(test): add get contract by id

--- tests/test_contract_repository.py
+++ tests/test_contract_repository.py
@@ -7,6 +7,7 @@ from psycopg_pool import AsyncConnectionPool
 
 from app.contract import Contract
 from app.contract_repository import ContractRepository
+from app.contract_status import ContractStatus
 from app.tables_truncater import TablesTruncater
 
 
@@ -29,3 +30,19 @@ async def test_contract_repository_save(
     contract_repository = ContractRepository(pool=database_pool)
     contract_id = await contract_repository.save(contract)
     assert contract_id == contract.contract_id
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_contract_repository_get_by_id(
+    database_pool: AsyncConnectionPool,
+) -> None:
+    contract = Contract(
+        amount=Decimal("1000"),
+        refundable_amount=Decimal("1000"),
+    )
+    contract_repository = ContractRepository(pool=database_pool)
+    contract_id = await contract_repository.save(contract)
+    recovered_contract = await contract_repository.get_by_id(contract_id)
+    assert contract == recovered_contract
+
+
