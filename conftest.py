081ce0ffe55420cd391029726e315f8da47f6497
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-04-15 12:26:24 -0300

    feat(test): add pytest conftest

---
+++ conftest.py
@@ -0,0 +1,18 @@
+from collections.abc import AsyncGenerator
+from typing import Any
+
+import pytest_asyncio
+from psycopg_pool import AsyncConnectionPool
+
+from app.environment_variables import environment_variables
+
+
+@pytest_asyncio.fixture(scope="session", loop_scope="session")
+async def database_pool() -> AsyncGenerator[AsyncConnectionPool[Any], None]:
+    async with AsyncConnectionPool(
+        conninfo=environment_variables.DATABASE_URL,
+        min_size=environment_variables.MINIMUM_POOL_SIZE,
+        max_size=environment_variables.MAXIMUM_POOL_SIZE,
+    ) as pool:
+        # logger.info("Creating database asynchronous pool...")
+        yield pool
