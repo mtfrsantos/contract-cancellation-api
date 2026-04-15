627f8d10c92e64a3e0dc2a04a7ee82366acb7721
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-04-15 12:28:26 -0300

    feat(test): add contract repository

---
+++ tests/test_contract_repository.py
@@ -0,0 +1,31 @@
+from collections.abc import AsyncGenerator
+from decimal import Decimal
+
+import pytest
+import pytest_asyncio
+from psycopg_pool import AsyncConnectionPool
+
+from app.contract import Contract
+from app.contract_repository import ContractRepository
+from app.tables_truncater import TablesTruncater
+
+
+@pytest_asyncio.fixture(autouse=True, loop_scope="session")
+async def before_each(
+    database_pool: AsyncConnectionPool,
+) -> AsyncGenerator[None, None]:
+    await TablesTruncater(pool=database_pool).execute()
+    yield
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_contract_repository_save(
+    database_pool: AsyncConnectionPool,
+) -> None:
+    contract = Contract(
+        amount=Decimal("1000"),
+        refundable_amount=Decimal("1000"),
+    )
+    contract_repository = ContractRepository(pool=database_pool)
+    contract_id = await contract_repository.save(contract)
+    assert contract_id == contract.contract_id
