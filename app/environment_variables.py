f21239f2e907b83c255be49155efd1399ff3203c
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-04-15 12:28:48 -0300

    feat: add environment variables manager

---
+++ app/environment_variables.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+
+class EnvironmentVariables:
+    def __init__(self) -> None:
+        _ = load_dotenv(override=True)
+
+    @property
+    def DATABASE_URL(self) -> str:
+        database_url = os.getenv("DATABASE_URL")
+        assert database_url, "DATABASE_URL should not be empty."
+        return str(database_url)
+
+    @property
+    def MINIMUM_POOL_SIZE(self) -> int:
+        return int(os.getenv("MINIMUM_POOL_SIZE") or 1)
+
+    @property
+    def MAXIMUM_POOL_SIZE(self) -> int:
+        return int(os.getenv("MAXIMUM_POOL_SIZE") or 1)
+
+
+environment_variables = EnvironmentVariables()
