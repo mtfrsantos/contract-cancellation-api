3636098b2e6defc1a86a9c51806fb4d2333e6e6c
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-04-15 16:10:59 -0300

    feat: add get contract by id

--- app/contract_repository.py
+++ app/contract_repository.py
@@ -3,8 +3,12 @@ from typing import Any
 from psycopg_pool import AsyncConnectionPool
 
 from app.contract import Contract
+from app.contract_status import ContractStatus
 from app.database import Database
 from app.insert_contract_query_factory import InsertContractQueryFactory
+from app.select_contract_by_id_query_factory import (
+    SelectContractByIdQueryFactory,
+)
 
 
 class ContractRepository:
@@ -18,6 +22,13 @@ class ContractRepository:
         single_result = self.get_single_result(result)
         contract_id_uuid = single_result.get("contract_id") or ""
         return str(contract_id_uuid)
+
+    async def get_by_id(self, contract_id: str) -> Contract:
+        query = SelectContractByIdQueryFactory().execute(contract_id)
+        result = await self._database.execute_query(query)
+        assert result, "Result should never be empty."
+        single_result = self.get_single_result(result)
+        return Contract.restore(**single_result)
 
     def get_single_result(
         self, result: list[dict[str, Any]]
---
+++ app/select_contract_by_id_query_factory.py
@@ -0,0 +1,18 @@
+from app.query import Query
+
+
+class SelectContractByIdQueryFactory:
+    def execute(self, contract_id: str) -> Query:
+        statement = """
+        SELECT 
+            id AS contract_id,
+            amount,
+            refundable_amount,
+            status,
+            created_at,
+            updated_at
+        FROM contracts
+        WHERE id = %(contract_id)s;
+        """
+        parameters = {"contract_id": contract_id}
+        return Query(statement=statement, parameters=parameters)
