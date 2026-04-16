943d0d9ca863fab96f15ca7daf404c3fd72d7469
Matheus Freitas Santos <mtfrsantos@gmail.com>
2026-04-16 01:38:05 -0300

    feat: add initialization info

--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ from psycopg_pool import AsyncConnectionPool
 
 from app.contracts_controller import contracts_controller
 from app.environment_variables import environment_variables
+from app.logger import logger
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    logger.info("Application startup: Initializing resources...")
     app.state.database_pool = AsyncConnectionPool(
         conninfo=environment_variables.DATABASE_URL,
         min_size=environment_variables.MINIMUM_POOL_SIZE,
@@ -17,6 +19,8 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     )
     yield
     await app.state.database_pool.close()
+    logger.info("Application shutdown: Cleaning up resources...")
+    logger.info("Database pool closed.")
 
 
 app = FastAPI(lifespan=lifespan)
